3e424e5858803ce7a77336d2c90a0b9f5ad3f5a8
Gustaf Sjoberg <gs@pipsq.com>
2016-12-15 11:18:35 +0100

    Added "any".

--- include/oath.hrl
+++ include/oath.hrl
@@ -1,4 +1,10 @@
 -define(VALIDATORS, [
+        {any, [
+            fun oath_validators:empty_check/2,
+            fun oath_validators:equal_to/2,
+            fun oath_validators:not_equal_to/2,
+            fun oath_validators:custom_validators/2
+        ]},
         {string, [
             fun oath_validators:strip/2,
             fun oath_validators:empty_check/2,
@@ -16,15 +22,18 @@
             fun oath_converters:list_converter/2,
             fun oath_validators:min_length_validator/2,
             fun oath_validators:max_length_validator/2
+            fun oath_validators:custom_validators/2
         ]},
         {proplist, [
             fun oath_validators:empty_check/2,
             fun oath_validators:ruleset_proplist_validator/2
+            fun oath_validators:custom_validators/2
         ]},
         {map, [
             fun oath_validators:empty_check/2,
             fun oath_converters:map_converter/2,
             fun oath_validators:ruleset_map_validator/2
+            fun oath_validators:custom_validators/2
         ]},
         {binary, [
             fun oath_validators:empty_check/2,
@@ -32,6 +41,7 @@
             fun oath_validators:min_size_validator/2,
             fun oath_validators:max_size_validator/2,
             fun oath_validators:value_in_validator/2
+            fun oath_validators:custom_validators/2
         ]},
         {integer, [
             fun oath_validators:strip/2,
@@ -44,6 +54,7 @@
             fun oath_validators:equal_to/2,
             fun oath_validators:not_equal_to/2,
             fun oath_validators:value_in_validator/2
+            fun oath_validators:custom_validators/2
         ]},
         {float, [
             fun oath_validators:strip/2,
@@ -56,14 +67,17 @@
             fun oath_validators:equal_to/2,
             fun oath_validators:not_equal_to/2,
             fun oath_validators:value_in_validator/2
+            fun oath_validators:custom_validators/2
         ]},
         {url, [
             string,
             fun oath_validators:valid_url/2
+            fun oath_validators:custom_validators/2
         ]},
         {email, [
             string,
             fun oath_validators:valid_email/2
+            fun oath_validators:custom_validators/2
         ]}
         %% date
         %% datetime
